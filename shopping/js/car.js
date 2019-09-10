0c05209bb1dfa41205113af2aaa2a01e8765b3d1
VLF-Y <1196568250@qq.com>
2019-09-10 14:42:46 +0800

    购物车

---
+++ shopping/js/car.js
@@ -0,0 +1,132 @@
+   class Car {
+        constructor() {
+            this.url = "http://localhost/project/shopping/data/goods.json";
+            this.tbody = document.querySelector("tbody");
+            this.qx = document.querySelector('.qx');
+            this.remove = document.querySelector(".remove");
+            this.price = document.querySelector(".price");
+            this.money = 0;
+            this.load();
+            this.addEvent();
+        }
+        load() {
+            ajax({
+                url: this.url,
+                success: (res) => {
+                    this.res = JSON.parse(res);
+                    this.getCookie();
+                }
+            })
+        }
+        getCookie() {
+            this.goods = getCookie("goods") ? JSON.parse(getCookie("goods")) : [];
+            this.display()
+        }
+        display() {
+            var str = "";
+            for (var i = 0; i < this.res.length; i++) {
+                for (var j = 0; j < this.goods.length; j++) {
+                    if (this.res[i].goodsId == this.goods[j].id) {
+                        str += `<tr index="${this.res[i].goodsId}">
+                                    <td><input type="checkbox" class="cb" /></td>
+                                    <td><img class="center-block" src="${this.res[i].url}"></td>
+                                    <td>${this.res[i].name}</td>
+                                    <td>${this.res[i].price}</td>
+                                    <td><input type="number" value="${this.goods[j].num}" class="num" min=1></td>
+                                    <td>${this.res[i].price*this.goods[j].num}</td>
+                                    <td class="delete">删除</td>
+                                </tr>`
+                    }
+                }
+            }
+            this.tbody.innerHTML = str;
+        }
+        addEvent() {
+            this.tbody.addEventListener("click", (eve) => {
+                var e = eve || window.event;
+                var tar = e.target || e.srcElement;
+                if (tar.className == "delete") {
+                    this.id = tar.parentNode.getAttribute("index");
+                    tar.parentNode.remove();
+                    this.setCookie((i) => {
+                        this.goods.splice(i, 1);
+                        this.setMoney();
+                    });
+                }
+                //复选框选中算价格
+                if (tar.className == 'cb') {
+                    this.td = tar.parentNode.parentNode.children;
+                    // console.log(this.td[0].children[0].checked);
+                    if (this.td[0].children[0].checked == true) {
+                        this.money += parseInt(this.td[5].innerHTML);
+                        this.price.innerHTML = this.money;
+                    } else {
+                        this.qx.checked = false;
+                        this.money -= parseInt(this.td[5].innerHTML);
+                        this.price.innerHTML = this.money;
+                    }
+                }
+            });
+            this.tbody.addEventListener("input", (eve) => {
+                var e = eve || window.event;
+                var tar = e.target || e.srcElement;
+                if (tar.className == "num") {
+                    this.val = tar.value;
+                    this.id = tar.parentNode.parentNode.getAttribute("index");
+                    this.td = tar.parentNode.parentNode.children;
+                    this.setCookie((i) => {
+                        this.goods[i].num = this.val;
+                        this.td[5].innerHTML = this.val * this.td[3].innerHTML;
+                        console.log(this.val,this.td[3].innerHTML);
+                        this.setMoney();
+                        // this.money += parseInt(this.val * this.td[3].innerHTML);
+                        // this.price.innerHTML = this.money;
+                    })
+                }
+            });
+            this.qx.addEventListener("click", (eve) => {
+                this.money = 0;
+                for (var i = 0; i < this.goods.length; i++) {
+                    this.tbody.children[i].children[0].children[0].checked = this.qx.checked;
+                    if (this.qx.checked) {
+                        this.money += parseInt(this.tbody.children[i].children[5].innerHTML);
+                    }
+                }
+                this.price.innerHTML = this.money;
+            });
+            this.remove.onclick = () => {
+                var t = confirm("确认清空购物车吗?");
+                if (t) {
+                    removeCookie("goods");
+                    this.getCookie();
+                }
+            }
+        }
+        setMoney() {
+            this.money = 0;
+            for (var i = 0; i < this.goods.length; i++) {
+                // console.log(this.tbody.children[i].children[0].children[0].checked);
+                if (this.tbody.children[i].children[0].children[0].checked) {
+                    this.money += parseInt(this.tbody.children[i].children[5].innerHTML);
+                }
+                // this.tbody.children[i].children[0].children[0].checked = this.qx.checked;
+                // console.log(this.tbody.children[i].children[5].innerHTML);
+                // if (this.qx.checked) {
+                //     this.money += parseInt(this.tbody.children[i].children[5].innerHTML);
+                // }
+            }
+            this.price.innerHTML = this.money;
+        }
+
+        setCookie(fn) {
+            for (var i = 0; i < this.goods.length; i++) {
+                if (this.goods[i].id == this.id) {
+                    fn(i);
+                }
+            }
+            setCookie("goods", JSON.stringify(this.goods), {
+                expires: 1000
+            });
+        }
+    }
+    new Car();
---
+++ shopping/js/list.js
@@ -0,0 +1,104 @@
+class List {
+        constructor() {
+            this.url = "http://localhost/project/shopping/data/goods.json";
+            this.box = document.querySelector(".box");
+
+            this.load();
+            // 1.准备绑定点击事件
+            this.addEvent();
+        }
+        load() {
+            var that = this;
+            ajax({
+                url: this.url,
+                success: function (res) {
+                    that.res = JSON.parse(res)
+                    that.display();
+                }
+            })
+        }
+        display() {
+            // console.log(this.res);
+            var str = "";
+            for (var i = 0; i < this.res.length; i++) {
+                str += `<div class="goods" index="${this.res[i].goodsId}">
+                            <img src="${this.res[i].url}" alt="">
+                            <p>${this.res[i].name}</p>
+                            <span>￥${this.res[i].price}</span>
+                            <em>${this.res[i].tip}</em>
+                            <i class="btn">加入购物车</i>
+                        </div>`
+            }
+            this.box.innerHTML = str;
+        }
+        addEvent() {
+            var that = this;
+            this.box.addEventListener("click", function (eve) {
+                var e = eve || window.event;
+                var target = e.target || e.srcElement;
+                if (target.className == "btn") {
+                    // 2.提前找到点击商品的唯一的id
+                    that.id = target.parentNode.getAttribute("index");
+                    // 3.准备存储localstorage
+                    that.setLocal();
+                }
+            })
+        }
+        setLocal() {
+
+            // 数据的格式：[{id:,num:,},{},{}]
+            // 先读
+            this.goods = getCookie("goods") ? JSON.parse(getCookie("goods")) : [];
+            console.log(this.goods);
+
+            if (this.goods.length < 1) {
+                // 第一次存，直接存
+                this.goods.push({
+                    id: this.id,
+                    num: 1
+                })
+            } else {
+                // 准备存重复数据的索引
+                var i;
+                // 利用some遍历数组，检测是否有重复数据
+                var o = this.goods.some((val, index) => {
+                    // 每次将索引，保存出来，
+                    i = index;
+                    // 如果id重复了，返回true，同时some自动停止
+                    return val.id == this.id;
+                })
+                // 如果some的返回值为true，表示肯定有一个值为true，那么表示有重复
+                if (o) {
+                    this.goods[i].num++
+                } else { //否则没有重复，再新增数据
+                    this.goods.push({
+                        id: this.id,
+                        num: 1
+                    })
+                }
+                var onoff = true;
+                for(var i=0;i<this.goods.length;i++){
+                    if(this.goods[i].id === this.id){
+                        // 存老的，老数据+1
+                        this.goods[i].num ++;
+                        onoff = false;
+                    }
+                }
+                if(onoff){
+                    // 存新的，增加新数据
+                    this.goods.push({
+                        id:this.id,
+                        num:1
+                    })
+                }
+            }
+
+            setCookie("goods", JSON.stringify(this.goods), {
+                expires: 1000
+            });
+        }
+    }
+
+    new List();
+
+    
