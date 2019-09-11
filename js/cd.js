5354684c5931a84c3d034e78e42839bef3b4304e
VLF-Y <1196568250@qq.com>
2019-09-11 15:49:10 +0800

    放大镜+二级菜单

--- js/cd.js
+++ js/cd.js
@@ -1,16 +1,9 @@
-$(function () {
-    //     $(".menu").children(".mainCate").children(".mainCate").mouseenter(function(){ 
-    //        $(".menu").children(".mainCate").children(".subCate").children(".subCate").stop().hide(400);
-    //        $(this).children(".subCate").stop().show(400); 
-    //     })
-
-
-    // let mainall = document.getElementsByClassName('mainCate');
-
-    // console.log(mainall)
-    // for (let i = 0; i < mainall.length; i++) {
-
-    // }
-
-    console.log($(".mainCate").)
-})
+; (function ($) {
+    $(".mainCate").mouseenter(function () {
+        $(".mainCate").find('.subCate').stop().hide();
+        $(this).find('.subCate').stop().show();
+    });
+    $(".mainCate").mouseleave(function () {
+        $(".mainCate").find('.subCate').stop().hide();
+    });
+})(jQuery);
